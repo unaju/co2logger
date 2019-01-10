5a6a53a1cd5bba24e5d2245c906bf07be88c9647
unaju <ElectricMoray@gmail.com>
2019-01-10 23:21:01 +0900

    有効な日付範囲を返すcgiを作成

---
+++ webview/src/docroot/get_date_range.rb
@@ -0,0 +1,17 @@
+#!/usr/local/bin/ruby
+# 設定可能な日付の範囲をjsonで返す
+require 'cgi'
+
+def get_range()
+  r = Dir["/var/co2log/*-*.csv"].
+    collect{ |v| v =~ %r!/(co2|temp)-(\d{6}).csv$! && $2 }.
+    find_all{ |v| v }.minmax
+  r.empty?() ? nil : r.collect{ |v| v.scan(/../).join("-") }
+end
+
+cgi = CGI.new
+cgi.out(type: 'application/json', charset: 'UTF-8') do
+  r = get_range
+  r ? "[\"#{r[0]}\",\"#{r[1]}\"]" : "null"
+end
+
