09bc91343fbd9733429eecd45a9465cfc0e09702
unaju <ElectricMoray@gmail.com>
2020-02-02 01:49:15 +0900

    コミット漏れ対処

--- co2logger/co2logger.rb
+++ co2logger/co2logger.rb
@@ -2,20 +2,69 @@
 require_relative 'co2dev'
 
 POLLING_SPAN = 2.0 # [s]
+WRITE_SPAN = 5 * 60 # [s]
 
 dev = Co2Dev.new()
 dev.open()
 
-while true
-  res = dev.read()
-  valfmt = {:temp => "%.2f", :co2 => "%d"}
-  [:temp, :co2].each { |key|
-    next unless res[key]
+class DataLogger
+  def get_outfile(t = Time.now)
+    "/var/co2log/#{@key}-" + t.strftime("%y%m%d") + ".csv"
+  end
+
+  def initialize(key, format)
+    @key = key
+    @format = format
+    @ofile = get_outfile()
+    @buffer = ""
+  end
+
+  def add(data)
     t = Time.now
-    file = "/var/co2log/" + t.strftime("#{key}-%y%m%d.csv")
-    data = "#{t.to_i},#{valfmt[key] % res[key]}\n"
-    File.open(file,'a') {|fd| fd.write(data) }
+    data = "#{t.to_i},#{@format % data}\n"
+    
+    # 出力ファイル名が変わる場合はbuffer書き出し
+    ofile2 = get_outfile(t)
+    if @ofile != ofile2
+      wo_buffer
+      @ofile = ofile2
+    end
+
+    # データをbufferに追記
+    @buffer += data
+  end
+
+  # bufferをファイルに書き出す
+  def wo_buffer()
+    unless @buffer.empty?
+      File.open(@ofile,'a') {|fd| fd.write(@buffer) }
+      @buffer = ""
+    end
+  end
+end
+
+
+keys = [:temp, :co2]
+logger = {
+  :temp => DataLogger.new("temp", "%.2f"),
+  :co2 => DataLogger.new("co2", "%d")
+}
+t0 = Time.now
+
+while true
+  # 読み出し
+  res = dev.read()
+
+  # 所定時間ごとに1回書き出しのflag
+  bufout = (Time.now - t0) > WRITE_SPAN
+  if bufout then t0 = Time.now end
+
+  # 書き出しbufferに追加
+  keys.each { |key|
+    if res[key] then logger[key].add(res[key]) end
+    if bufout then logger[key].wo_buffer() end
   }
+
   sleep(POLLING_SPAN)
 end
 
